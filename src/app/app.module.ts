ccd4b238ed885f6a4af9912a0f5e89500aaef0b5
arif.khan <arif.khan@bakingo.com>
2018-04-09 17:10:45 +0530

    Shared module integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
+import { SharedModule } from './shared/shared.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +12,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
